5ed677a7233194fd27aacc961ac384cc8438be21
Rafal Pasko <rp.261000@gmail.com>
2024-03-14 11:02:14 +0100

    Serialize, Deserialize Proto structs.

--- build.rs
+++ build.rs
@@ -1,4 +1,12 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/kvp_store.proto")?;
+    tonic_build::configure()
+    .type_attribute("KvpKey", "#[derive(serde::Deserialize, serde::Serialize)]")
+    .type_attribute("KvpPayload", "#[derive(serde::Deserialize, serde::Serialize)]")
+    .type_attribute("KvpResponse", "#[derive(serde::Deserialize, serde::Serialize)]")
+    .compile(
+        &["proto/kvp_store.proto"],
+        &["proto"],
+    )?;
+    
     Ok(())
 }
